b91846c7f02b8ae86b1d12f5b3fdd90e0812c684
gagip94 <gagip94@ttam.ai>
2024-07-05 09:55:22 +0900

    feat: 페어링 및 설치 탭 분리

--- app.py
+++ app.py
@@ -1,14 +1,37 @@
-import subprocess
 import os
-from flask import Flask, render_template, request
+import subprocess
+
+from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__)
 
 
-def run_commend(commend):
+def run_command(command):
     result = subprocess.run(
-        commend, capture_output=True, text=True, check=True)
+        command, capture_output=True, text=True, check=True)
     return result.stdout
+
+
+def adb_process(commands):
+    results = {}
+    try:
+        kill_server_result = run_command(['adb', 'kill-server'])
+        results['kill-server'] = kill_server_result
+
+        start_server_result = run_command(['adb', 'start-server'])
+        results['start-server'] = start_server_result
+
+        for command in commands:
+            command_result = run_command(command)
+            results[command[0]] = command_result
+
+    except subprocess.CalledProcessError as e:
+        results['error'] = f"An error occurred: {e.stderr}"
+
+    except Exception as e:
+        results['error'] = f"An unexpected error occurred: {e}"
+
+    return results
 
 
 @app.route('/')
@@ -17,49 +40,43 @@ def home():
         file_names = os.listdir('files')
     except FileNotFoundError:
         file_names = []
-    return render_template('home.html', options=file_names)
+    # 내림차순 정렬
+    sorted_file_names = sorted(file_names, reverse=True)
+    return render_template('home.html', options=sorted_file_names)
+
+
+@app.route('/pair', methods=['POST'])
+def pair():
+    ip_address = request.form['ipAddress']
+    pairing_code = request.form['pairingCode']
+    if not ip_address:
+        return jsonify({'error': 'IP address is required.'})
+    if not pairing_code:
+        return jsonify({'error': 'Pairing code is required.'})
+
+    commands = [
+        ['adb', 'pair', ip_address, pairing_code],
+        ['adb', 'connect', ip_address]
+    ]
+    results = adb_process(commands)
+    return jsonify(results)
 
 
 @app.route('/install', methods=['POST'])
-def process():
+def install():
     selected_option = request.form['options']
     ip_address = request.form['ipAddress']
-    pairing_code = request.form['pairingCode']
-    results = {}
-
-    try:
-        kill_server_result = run_commend(
-            ['adb', 'kill-server'])
-        results['kill-server'] = kill_server_result
-
-        start_server_result = run_commend(
-            ['adb', 'start-server'])
-        results['start-server'] = start_server_result
-
-        if pairing_code:
-            pair_result = run_commend(
-                ['adb', 'pair', ip_address, pairing_code])
-            results['pair'] = pair_result
-
-        connect_result = run_commend(
-            ['adb', 'connect', ip_address])
-        results['connect'] = connect_result
-
-        install_result = run_commend(
-            ['adb', 'install', f'./files/{selected_option}'])
-        results['install'] = install_result
-
-    except subprocess.CalledProcessError as e:
-        results['error'] = f"An error occurred: {e.stderr}"
-
-    except Exception as e:
-        results['error'] = f"An unexpected error occurred: {e}"
-
-    return render_template(
-        'home.html',
-        options=os.listdir('files'),
-        results=results
-    )
+    if not ip_address:
+        return jsonify({'error': 'IP address is required.'})
+    if not selected_option:
+        return jsonify({'error': 'An option is required.'})
+    
+    commands = [
+        ['adb', 'connect', ip_address],
+        ['adb', 'install', f'./files/{selected_option}']
+    ]
+    results = adb_process(commands)
+    return jsonify(results)
 
 
 if __name__ == '__main__':
